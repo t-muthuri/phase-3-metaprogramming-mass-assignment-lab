81a1159e5050e1b8de33fc620e6e9b6d458ed14a
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-11-03 08:49:43 +0300

    Completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,15 @@
 class Person
-  # your code here
+
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :wrist_size, :glove_size, :pant_length, :pant_width, :weight, :handed, :complexion, :t_shirt_size
+  # attr_accessor :weight, :handed, :complexion, :t_shirt_size
+  # attr_accessor :wrist_size, :glove_size, :pant_length, :pant_width
+
+  def initialize (attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+      # send mehod mass assigns the value of each key/value pair to its associaed key
+    end
+  end
+
 end
